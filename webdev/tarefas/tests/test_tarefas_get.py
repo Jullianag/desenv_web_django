73415475c192580076d089983a0af1500c836abd
jullianag <juh_g@hotmail.com>
2022-04-30 11:42:26 -0300

    criada lista de tarefas pendentes
    close #5

--- webdev/tarefas/tests/test_tarefas_get.py
+++ webdev/tarefas/tests/test_tarefas_get.py
@@ -1,12 +1,13 @@
 import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertContains
+from webdev.tarefas.models import Tarefa
 
 
 
 @pytest.fixture
 # o código se repete, por isso foi criada a função resposta e fixture
-def resposta(client):
+def resposta(client, db):
     # desse jeito o teste não fica dependende do endereço escrito
     resp = client.get(reverse('tarefas:home'))
     return resp
@@ -21,6 +22,29 @@ def test_formulario_presente(resposta):
     #  garantimos que na resposta deve vir a tag de '<form'
     assertContains(resposta, '<form')
 
+
 # garante a existencia do botão
 def test_botao_salvar_presente(resposta):
     assertContains(resposta, '<button type="submit"')
+
+
+@pytest.fixture
+def lista_de_tarefas_pendentes(db):
+    tarefas = [
+        Tarefa(nome='Tarefa 1', feita=False),
+        Tarefa(nome='Tarefa 2', feita=False),
+    ]
+    Tarefa.objects.bulk_create(tarefas)
+    return tarefas
+
+
+@pytest.fixture
+def resposta_com_lista_de_tarefas(client, lista_de_tarefas_pendentes):
+    resp = client.get(reverse('tarefas:home'))
+    return resp
+
+
+def test_lista_de_tarefas_pendentes_presentes(resposta_com_lista_de_tarefas, lista_de_tarefas_pendentes):
+    for tarefa in lista_de_tarefas_pendentes:
+        # ver se o nome de cada uma das tarefas da lista aparace
+        assertContains(resposta_com_lista_de_tarefas, tarefa.nome)
